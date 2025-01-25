5add8bc261f465176c626500bfffa272a8cdf0e9
Yaroslav SAKAL <yaroslav.sakal@univ-rouen.fr>
2025-01-25 15:27:36 +0100

    Corrected APIs, .pom file, working

---
+++ shop-server/src/main/java/com/example/shop_server/HelloController.java
@@ -0,0 +1,12 @@
+package com.example.shop_server;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/")
+    public String hello() {
+        return "Hello, World!";
+    }
+}
--- shop-server/src/main/java/com/example/shop_server/model/Category.java
+++ shop-server/src/main/java/com/example/shop_server/model/Category.java
@@ -1,6 +1,17 @@
 package com.example.shop_server.model;
 
-import jakarta.persistence.*;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+// import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
 import java.util.ArrayList;
 import java.util.Date;
